21e33f357673f76d0f239c9b333c7ff0ee0e6794
Jesse Gillies <jessegillies@adt.com>
2021-09-14 07:20:47 -0400

    chagnes

--- macros/get_profile.sql
+++ macros/get_profile.sql
@@ -17,47 +17,62 @@ set relation = adapter.get_relation(
 -%}
 
 {%- set columns = adapter.get_columns_in_relation(relation) -%}
-{%- set column_names = columns | map(attribute="name") -%}
 
+{% set num_columns=columns|length %}
 
+{% set column_groupings= (num_columns / 10 )|round|int%}
 
-{% set profile_sql %}
-  with column_profiles as (
-    {% for column_name in column_names %}
-      select 
-        lower('{{ column_name }}') as column_name,
-        sum(case when {{ adapter.quote(column_name) }} is null then 0 else 1 end) / cast(count(*) as numeric) as not_null_proportion,
-        count(distinct {{ adapter.quote(column_name) }}) / cast(count(*) as numeric) as distinct_proportion,
-        count(distinct {{ adapter.quote(column_name) }}) as distinct_count,
-        count(distinct {{ adapter.quote(column_name) }}) = count(*) as is_unique,
-        cast(max({{ adapter.quote(column_name) }}) as string) as max_value,
-        cast(min({{ adapter.quote(column_name) }}) as string) as min_value,
-        cast(current_timestamp as {{ dbt_profiler.type_string() }}) as profiled_at
-      from {{ relation }}
+{% set ns=namespace(columns=[], column_set_interval=0) %}
 
-      {% if not loop.last %}union all{% endif %}
-    {% endfor %}
-  )
-  select
-    column_profiles.column_name,
-    columns.data_type,
-    column_profiles.not_null_proportion,
-    column_profiles.distinct_proportion,
-    column_profiles.distinct_count,
-    column_profiles.is_unique,
-    column_profiles.max_value,
-    column_profiles.min_value,
-    column_profiles.profiled_at
-  from column_profiles
-  left join {{ dbt_profiler.information_schema(relation) }}.COLUMNS as columns on (
-    lower(columns.table_schema) = lower('{{ schema }}') and
-    lower(columns.table_name) = lower('{{ relation_name }}') and
-    lower(columns.column_name) = lower(column_profiles.column_name)
-  )
-{% endset %}
+{% for r in range(column_groupings+1)%}
+    {% set column_set = columns[ns.column_set_interval: ns.column_set_interval+10] %}
+    {% if column_set | length > 0 %}
+        
+        {% call statement('profile', fetch_result=True)%}
+          with column_profiles as (
+            {% for col in column_set %}
+              {% set column_name=col.name %}
+              select 
+                lower('{{ column_name }}') as column_name,
+                sum(case when {{ adapter.quote(column_name) }} is null then 0 else 1 end) / cast(count(*) as numeric) as not_null_proportion,
+                count(distinct {{ adapter.quote(column_name) }}) / cast(count(*) as numeric) as distinct_proportion,
+                count(distinct {{ adapter.quote(column_name) }}) as distinct_count,
+                count(distinct {{ adapter.quote(column_name) }}) = count(*) as is_unique,
+                cast(max({{ adapter.quote(column_name) }}) as string) as max_value,
+                cast(min({{ adapter.quote(column_name) }}) as string) as min_value,
+                cast(current_timestamp as {{ dbt_profiler.type_string() }}) as profiled_at
+              from {{ relation }}
 
-{% set results = run_query(profile_sql) %}
-{% set results = results.rename(results.column_names | map('lower')) %}
-{% do return(results) %}
+              {% if not loop.last %}union all{% endif %}
+            {% endfor %}
+          )
+          select
+            column_profiles.column_name,
+            columns.data_type,
+            column_profiles.not_null_proportion,
+            column_profiles.distinct_proportion,
+            column_profiles.distinct_count,
+            column_profiles.is_unique,
+            column_profiles.max_value,
+            column_profiles.min_value,
+            column_profiles.profiled_at
+          from column_profiles
+          left join {{ dbt_profiler.information_schema(relation) }}.COLUMNS as columns on (
+            lower(columns.table_schema) = lower('{{ schema }}') and
+            lower(columns.table_name) = lower('{{ relation_name }}') and
+            lower(columns.column_name) = lower(column_profiles.column_name)
+          )
+        {% endcall %}
+    {% endif %}
+
+    {% set results=load_result('profile') %}
+    {% set profile_data=results['data'] %}
+    {% set ns.columns = ns.columns + profile_data %}
+
+    {% set ns.column_set_interval=ns.column_set_interval+10 %}
+
+{%endfor%}
+
+{% do return(ns.columns) %}
 
 {% endmacro %}
